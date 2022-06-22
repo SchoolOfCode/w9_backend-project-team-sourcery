3a27b2dd18f7a05db3b3791a1e1f73a5fbf4b6c9
AfamOkoh <afamokoh@gmail.com>
2022-06-22 11:32:17 +0100

    ensured post request was using req body properly

--- routes/resources.js
+++ routes/resources.js
@@ -5,13 +5,14 @@ import { getAllResources, addToResources } from "../models/resources.js";
 /* GETs all  resources. */
 router.get("/", async function (req, res) {
   const result = await getAllResources();
+  console.log(result, "get request");
   res.json(result);
 });
 
 /* Adds a row to resources table */
 router.post("/", async function (req, res) {
-  const resource = req;
-  console.log(resource.body);
+  const resource = req.body;
+  console.log(resource, "resource posted");
   const result = await addToResources(resource);
   res.json({ success: true });
 });
