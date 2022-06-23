fac99b1653325f4de212712f801b69c31c35f088
AfamOkoh <afamokoh@gmail.com>
2022-06-23 12:30:49 +0100

    fixed small errors after merge

--- db/scripts/resources/seedTable.js
+++ db/scripts/resources/seedTable.js
@@ -8,15 +8,19 @@ async function populateTable() {
       url: "http://www.postman.com",
       description: "this is postman, great for sorting out routes",
       likes: 4,
-      week: 1
+      week: 1,
     },
   ];
   for (let i = 0; i < test.length; i++) {
     const response = await pool.query(
-
       `INSERT INTO resources (name, url, description, likes, week) VALUES ($1, $2, $3, $4, $5) RETURNING *;`,
-      [test[i].name, test[i].url, test[i].description, test[i].likes, test[i].week
-      
+      [
+        test[i].name,
+        test[i].url,
+        test[i].description,
+        test[i].likes,
+        test[i].week,
+      ]
     );
     console.log(response.rows);
   }
